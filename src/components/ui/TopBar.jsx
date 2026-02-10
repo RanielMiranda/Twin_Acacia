0e8e38c10f43f8c1d5a08dd363856c1cc008239d
Astariskk <mirandaraniel89@gmail.com>
2026-02-10 17:29:37 +0800

    Update TopBar.jsx

--- src/components/ui/TopBar.jsx
+++ src/components/ui/TopBar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { HashLink } from "react-router-hash-link";
-import ContactModal from "./Modals/ContactModal";
+import ContactModal from "./modals/ContactModal";
 
 export default function TopBar() {
   const [isModalOpen, setIsModalOpen] = useState(false);
